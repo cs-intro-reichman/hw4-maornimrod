9752b5ad39db121a80995c824e2b9401f6b8f539
maornimrod <maorhar1@gmail.com>
2024-11-29 12:18:50 +0200

    HM4

--- MyString.java
+++ MyString.java
@@ -9,7 +9,7 @@ public class MyString {
         System.out.println("Testing contains:");
         System.out.println(contains("unhappy", "happy")); // true
         System.out.println(contains("happy", "unhappy")); // false
-        System.out.println(contains("historical", "story")); // false
+        System.out.println(contains("historical", "story")); // false**
         System.out.println(contains("psychology", "psycho")); // true
         System.out.println(contains("personality", "son")); // true
         System.out.println(contains("personality", "dad")); // false
@@ -18,13 +18,36 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        for(int i =0; i<str.length(); i++){
+            if(str.charAt(i)>64 && str.charAt(i)<91){
+                str = str.replace(str.charAt(i), (char) (str.charAt(i)+32));
+            }
+        }
+        return str;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        int begin =0;
+        boolean contain = true;
+        for(int i =0; i<str1.length(); i++){
+            if(str1.charAt(i) == str2.charAt(0)){
+                begin = i;
+                break;
+            }
+        }
+        if(str1.charAt(begin) == str2.charAt(0)){
+            for(int j = 0; j<str2.length(); j++){
+                if(str1.charAt(begin) != str2.charAt(j)){
+                    contain = false;
+                    break;
+                }
+                begin++;
+            }
+        }
+        else{
+            return false;
+        }
+        return contain;
     }
 }
